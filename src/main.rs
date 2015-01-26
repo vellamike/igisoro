cec630c6042be72a8fd566b772c34d71bbfb6a45
Mike Vella <vellamike@gmail.com>
2015-01-26 01:00:51 +0000

    Figured out multi dimensional arrays

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// Early attempt at an igisoro implementation
+
+
+fn main() {
+    let mut player_1_state = [[0;8]; 2];
+    let mut player_2_state = [[0;8]; 2];
+    println!("{}", player_1_state[0][0]);
+}
