37380fa4ce603575ac5ecfb6672873a1e9e87577
Mike Vella <vellamike@gmail.com>
2015-01-27 21:24:02 +0000

    Initializer which picks valid igisoro starting positions, at random

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@ use std::rand;
 use std::rand::Rng;
 
 fn rand_int() -> u8 {
-    let mut rng = rand::thread_rng();
-    rng.gen::<u8>()
+      rand::thread_rng().gen_range(1u8, 10)
 }
 
-fn random_initalizer(n:u8) -> [[u8;8];2] {
+fn random_initializer(n:u8) -> [[u8;8];2] {
     let mut example_state = [[n;8]; 2];
     for i in example_state.iter_mut() {
         for j in i.iter_mut(){
@@ -18,6 +17,19 @@ fn random_initalizer(n:u8) -> [[u8;8];2] {
     }
     example_state
 }
+
+fn smart_random_initializer(mut num_seeds:u8, x:uint, y:uint) -> [[u8;8];2] {
+
+    let mut example_state = [[0;8]; 2 ]; //haven't figured out how to assign dimensions dynamically to an array - I suspect its not possible, for now work with x = 8, y = 2
+    
+    for i in range(0, num_seeds){
+        let x = rand::thread_rng().gen_range(0, x);
+        let y = rand::thread_rng().gen_range(0, y);
+        example_state[y][x] += 1;
+    }
+    example_state
+}
+
 
 
 fn uniform_initalizer(n:u8) -> [[u8;8];2] {
@@ -39,13 +51,8 @@ fn display_state(a:&[[u8;8];2]){
 }
 
 fn main() {
-    let mut player_1_state = [[0;8]; 2];
-    display_state(&player_1_state);
-
-    let j = uniform_initalizer(2);
-    display_state(&j);
-
-    let p = random_initalizer(2);
-    display_state(&p);
-
+    let player_1_state = smart_random_initializer(8,8,2);
+    display_state(&player_1_state);    
+    let player_2_state = smart_random_initializer(8,8,2);
+    display_state(&player_2_state);    
 }
